024b4d78dcb87ab2308f1893f366acbe2208c8ee
ParthDarade <71880321+ParthDarade@users.noreply.github.com>
2021-06-20 17:45:11 +0530

    Add files via upload

---
+++ sketch_jun20a.ino
@@ -0,0 +1,17 @@
+bool switchState;
+void setup() {
+  Serial.begin(9600);
+  pinMode(7,INPUT);
+}
+
+void loop() {
+ if(digitalRead(7)==LOW){
+  if(switchState=true){
+  Serial.print("Y");
+  }
+  switchState=false;
+  }
+  if(digitalRead(7)== HIGH){
+    switchState=true;
+  }
+}
